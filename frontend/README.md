bbde4559e4833e6715ab90f30beac8bc082823f2
David Reagan <dmreagan@iu.edu>
2017-03-30 19:14:29 -0400

    Clean up js to make eslint happier. Update readme.

--- frontend/README.md
+++ frontend/README.md
@@ -4,7 +4,7 @@ The frontend is written in [AngularJS](https://angularjs.org/) (1.4.8).
 
 The single file `app.js` contains all the application logic.
 
-## Serve application
+## Serve application the simple way
 
 The simplest way to serve the frontend is by using the Python
 [SimpleHTTPServer](https://docs.python.org/2/library/simplehttpserver.html).
@@ -16,3 +16,13 @@ $ python -m SimpleHTTPServer
 
 Then open the application on port `8000` in the browser:
 `http://localhost:8000/index.html`
+
+## Serve application the awesome way
+
+In order to get auto-reloading on file save, you can serve the frontend with browser-sync.
+
+```console
+$ npm run serve
+```
+
+This will open a browser window to port 3000.
--- frontend/app.js
+++ frontend/app.js
@@ -1,92 +1,81 @@
-'use strict';
-
 angular.module('app', [])
   .value('apiHost', 'http://localhost:8080')
-  .service('apiUrl', function(apiHost) {
-    return function(path) {
-      return apiHost + path;
-    }
+  .service('apiUrl', (apiHost) => function (path) {
+    return apiHost + path;
   })
-  .service('Api', function(apiUrl, $q, $http) {
-    return function(path) {
-      var url = apiUrl(path);
-      this.post = function(data) {
+  .service('Api', (apiUrl, $q, $http) => function (path) {
+    const url = apiUrl(path);
+    this.post = function (data) {
         // CREATE
-        return $http.post(url, JSON.stringify(data));
-      };
-      this.get = function() {
+      return $http.post(url, JSON.stringify(data));
+    };
+    this.get = function () {
         // RETRIEVE
-        return $http.get(url);
-      };
-      this.put = function(data) {
+      return $http.get(url);
+    };
+    this.put = function (data) {
         // UPDATE
-        return $http.put(url, JSON.stringify(data));
-      };
-      this.delete = function() {
+      return $http.put(url, JSON.stringify(data));
+    };
+    this.delete = function () {
         // DELETE
-        return $http.delete(url);
-      };
-    }
+      return $http.delete(url);
+    };
   })
-  .service('Location', function($q) {
-    var getLocation = function(success, error) {
+  .service('Location', ($q) => {
+    const getLocation = function (success, error) {
       if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(success);
       } else {
         error(new Error('Your browser does not support HTML5 location.'));
       }
     };
-    return function() {
-      this.get = function() {
-        var d = $q.defer();
-        getLocation(function(position){
+    return function () {
+      this.get = function () {
+        const d = $q.defer();
+        getLocation((position) => {
           d.resolve(position);
-        }, function(e){
+        }, (e) => {
           d.reject(e);
         });
         return d.promise;
       };
-    }
-  })
-  .service('objectArrayIndexOf', function() {
-    return function(arr, k, v) {
-      // usage: objectArrayIndexOf([{id:2},{id:1},{id:3}], 'id', 1) -> 1
-      for (var i = 0; i < arr.length; i++) {
-        if (arr[i][k] === v) return i;
-      }
-      return -1;
     };
   })
-  .directive("blob", function() {
-    return {
-      scope: {
-        blob: "="
-      },
-      link: function($scope, elem, attr) {
-        elem.bind("change", function(changeEvent) {
-          var reader = new FileReader();
-          reader.onload = function(loadEvent) {
-            $scope.$apply(function() {
-              $scope.blob = loadEvent.target.result;
-            });
-          }
-          reader.readAsBinaryString(changeEvent.target.files[0]);
-        });
-      }
+  .service('objectArrayIndexOf', () => function (arr, k, v) {
+      // usage: objectArrayIndexOf([{id:2},{id:1},{id:3}], 'id', 1) -> 1
+    for (let i = 0; i < arr.length; i += 1) {
+      if (arr[i][k] === v) return i;
     }
+    return -1;
   })
-  .controller('GuestbookController', function($scope, $q, apiHost, Api, Location, objectArrayIndexOf) {
-
-    var EMPTY = {
+  .directive('blob', () => ({
+    scope: {
+      blob: '=',
+    },
+    link($scope, elem, attr) {
+      elem.bind('change', (changeEvent) => {
+        const reader = new FileReader();
+        reader.onload = function (loadEvent) {
+          $scope.$apply(() => {
+            $scope.blob = loadEvent.target.result;
+          });
+        };
+        reader.readAsBinaryString(changeEvent.target.files[0]);
+      });
+    },
+  }))
+  .controller('GuestbookController', function ($scope, $q, apiHost, Api, Location, objectArrayIndexOf) {
+    const EMPTY = {
       user: {
         name: null,
-        location: null
+        location: null,
       },
       text: null,
-      image_ref: null
+      image_ref: null,
     };
-    var locationCache = null;
-    var SEARCH_API = new Api('/search');
+    const locationCache = null;
+    const SEARCH_API = new Api('/search');
 
     $scope.posts = [];
     $scope.projects = [];
@@ -94,10 +83,30 @@ angular.module('app', [])
     $scope.formdata = angular.copy(EMPTY);
     $scope.apiHost = apiHost;
     $scope.search = {
-      query_string: ''
+      query_string: '',
     };
 
-    var init = function() {
+    const loadPosts = function () {
+      const api = new Api('/posts');
+      api.get().then((response) => {
+        $scope.posts = response.data;
+      }, (e) => {
+        console.warn(e);
+        $scope.posts = [];
+      });
+    };
+
+    const loadProjects = function () {
+      const api = new Api('/projects');
+      api.get().then((response) => {
+        $scope.projects = response.data;
+      }, (e) => {
+        console.warn(e);
+        $scope.projects = [];
+      });
+    };
+
+    const init = function () {
       // // try to get location from browser
       // var location = new Location();
       // location.get().then(function(pos) {
@@ -113,60 +122,40 @@ angular.module('app', [])
       loadProjects();
     };
 
-    var loadPosts = function() {
-      var api = new Api('/posts');
-      api.get().then(function(response) {
-        $scope.posts = response.data;
-      }, function(e) {
-        console.warn(e);
-        $scope.posts = [];
-      });
-    };
-
-    var loadProjects = function() {
-      var api = new Api('/projects');
-      api.get().then(function(response) {
-        $scope.projects = response.data;
-      }, function(e) {
-        console.warn(e);
-        $scope.projects = [];
-      });
-    };
-
     // reset input form but refill location from cache
-    var resetForm = function() {
+    const resetForm = function () {
       $scope.formdata = angular.copy(EMPTY);
       $scope.formdata.user.location = angular.copy(locationCache);
       $scope.imagedata = null;
     };
 
-    var uploadBlob = function(blob) {
-      var d = $q.defer();
-      var api = new Api('/images');
-      api.post({'blob': btoa(blob)}).then(function(response) {
+    const uploadBlob = function (blob) {
+      const d = $q.defer();
+      const api = new Api('/images');
+      api.post({ blob: btoa(blob) }).then((response) => {
         d.resolve(response);
-      }, function(response) {
+      }, (response) => {
         if (response.status === 409) {
           d.resolve(response);
         } else {
           d.reject(response);
         }
-      })
+      });
       return d.promise;
     };
 
-    var submitPost = function() {
-      var api = new Api('/posts');
-      api.post($scope.formdata).then(function(response) {
-        var posts = response.data;
-        for (var i=0; i<posts.length; i++) {
+    const submitPost = function () {
+      const api = new Api('/posts');
+      api.post($scope.formdata).then((response) => {
+        const posts = response.data;
+        for (let i = 0; i < posts.length; i += 1) {
           $scope.posts.unshift(posts[0]);
         }
         resetForm();
-      }, function(e) {
+      }, (e) => {
         console.warn(e);
         window.alert('Creating the post failed.');
-      })
+      });
     };
 
     // var searchPosts = function() {
@@ -178,23 +167,21 @@ angular.module('app', [])
     //   });
     // };
 
-    var searchProjects = function() {
-      SEARCH_API.post($scope.search).then(function(response) {
+    const searchProjects = function () {
+      SEARCH_API.post($scope.search).then((response) => {
         $scope.projects = response.data;
-      }, function(e) {
+      }, (e) => {
         console.warn(e);
         $scope.projects = [];
       });
     };
 
     // watch search input
-    $scope.$watch(function(scope) {
-      return scope.search.query_string;
-    }, function(newVal, oldVal) {
+    $scope.$watch(scope => scope.search.query_string, (newVal, oldVal) => {
       if (newVal === '') {
         // loadPosts();
         loadProjects();
-      } else if (newVal != oldVal) {
+      } else if (newVal !== oldVal) {
         // searchPosts();
         searchProjects();
       }
@@ -202,13 +189,13 @@ angular.module('app', [])
 
 
     // create new post
-    this.submitForm = function() {
+    this.submitForm = function () {
       if (!$scope.formdata.user.location) return;
       if ($scope.imagedata) {
-        uploadBlob($scope.imagedata).then(function(response) {
+        uploadBlob($scope.imagedata).then((response) => {
           $scope.formdata.image_ref = response.data.digest;
           submitPost();
-        }, function(e) {
+        }, (e) => {
           console.warn(e);
           window.alert('Image upload failed.');
         });
@@ -218,41 +205,40 @@ angular.module('app', [])
     };
 
     // like an existing post
-    this.likePost = function(post) {
-      var api = new Api('/post/' + post.id + '/like');
-      api.put().then(function(response) {
+    this.likePost = function (post) {
+      const api = new Api(`/post/${post.id}/like`);
+      api.put().then((response) => {
         post.like_count = response.data.like_count;
-      }, function(e) {
+      }, (e) => {
         console.warn(e);
         window.alert('Liking the post failed.');
       });
     };
 
     // edit existing post
-    this.editPost = function(post) {
+    this.editPost = function (post) {
       console.warn('editPost() is not implemented');
     };
 
     // delete existing post
-    this.deletePost = function(post) {
-      var idx = objectArrayIndexOf($scope.posts, 'id', post.id);
-      var api = new Api('/post/' + post.id);
-      api.delete().then(function(response) {
+    this.deletePost = function (post) {
+      const idx = objectArrayIndexOf($scope.posts, 'id', post.id);
+      const api = new Api(`/post/${post.id}`);
+      api.delete().then((response) => {
         $scope.posts.splice(idx, 1);
-      }, function(e){
+      }, (e) => {
         console.warn(e);
         window.alert('Deleting the post failed.');
       });
     };
 
     // clear search form
-    this.clearSearch = function() {
+    this.clearSearch = function () {
       $scope.search = {
-        query_string: ''
+        query_string: '',
       };
     };
 
     // initialize controller
     init();
-
   });
