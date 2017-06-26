9a04550af6e117c89e35bbb17dad31f7d75ad6b8
David Reagan <dmreagan@iu.edu>
2017-06-26 17:55:47 -0400

    Fetch names for people, campuses, categores, disciplines. #25.

--- frontend/project-detail/project-detail.component.js
+++ frontend/project-detail/project-detail.component.js
@@ -1,27 +1,98 @@
 angular
   .module('projectDetail')
   .component('projectDetail', {
-    // template: 'TBD: Detail view for <span>{{ $ctrl.projectId }}</span>',
     templateUrl: 'project-detail/project-detail.template.html',
     controller: ['$routeParams', 'Api',
       function ProjectDetailController($routeParams, Api) {
         this.projectId = $routeParams.projectId;
         this.project = {};
+        this.peopleObjects = [];
+        this.campuses = [];
+        this.campusNames = [];
+        this.categories = [];
+        this.categoryNames = [];
+        this.disciplines = [];
+        this.disciplineNames = [];
 
-        this.getProject = () => {
+        this.createPeopleObjects = () => {
+          const peopleStrings = this.project.people.split(';');
+
+          this.peopleObjects = peopleStrings.map((personString) => {
+            const personStringSplit = personString.split('--');
+
+            return {
+              name_first: personStringSplit[0],
+              name_last: personStringSplit[1],
+            };
+          });
+        };
+
+        this.createCampusNames = () => {
+          this.campusNames = this.project.campus_ids.map(
+            id => this.campuses.find(campus => campus.id === id).name,
+          );
+        };
+
+        this.createCategoryNames = () => {
+          this.categoryNames = this.project.category_ids.map(
+            id => this.categories.find(category => category.id === id).name,
+          );
+        };
+
+        this.createDisciplineNames = () => {
+          this.disciplineNames = this.project.discipline_ids.map(
+            id => this.disciplines.find(discipline => discipline.id === id).name,
+          );
+        };
+
+        const getCampuses = () => {
+          const api = new Api('/campuses');
+          api.get().then((response) => {
+            this.campuses = response.data;
+            this.createCampusNames();
+          }, (e) => {
+            console.warn(e);
+            this.campuses = [];
+          });
+        };
+
+        const getCategories = () => {
+          const api = new Api('/categories');
+          api.get().then((response) => {
+            this.categories = response.data;
+            this.createCategoryNames();
+          }, (e) => {
+            console.warn(e);
+            this.categories = [];
+          });
+        };
+
+        const getDisciplines = () => {
+          const api = new Api('/disciplines');
+          api.get().then((response) => {
+            this.disciplines = response.data;
+            this.createDisciplineNames();
+          }, (e) => {
+            console.warn(e);
+            this.disciplines = [];
+          });
+        };
+
+        const getProject = () => {
           const api = new Api(`/project/${this.projectId}`);
           api.get().then((response) => {
             this.project = response.data;
-
-            // // add campus info to the form for checkboxes
-            // $scope.formdata.campus_checkbox = angular.copy($scope.campuses);
+            this.createPeopleObjects();
+            getCampuses();
+            getCategories();
+            getDisciplines();
           }, (e) => {
             console.warn(e);
             this.project = {};
           });
         };
 
-        this.getProject();
+        getProject();
       },
     ],
   });
--- frontend/project-detail/project-detail.component.spec.js
+++ frontend/project-detail/project-detail.component.spec.js
@@ -5,7 +5,30 @@ describe('projectDetail', () => {
     let $httpBackend;
     let ctrl;
     const url = 'http://localhost:8080';
-    const mockProject = { campus_ids: [7, 1], category_ids: [8], discipline_ids: [15], tags: ['definition', 'Versatile'], id: 'fe4cb1d025bd7c4bbd7845ffd36a2f2045b246f5', title: 'Tin', description: 'Donec diam neque, vestibulum eget, vulputate ut, ultrices vel, augue. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec pharetra, magna vestibulum aliquet ultrices, erat tortor sollicitudin mi, sit amet lobortis sapien sapien non mi. Integer ac neque.\n\nDuis bibendum. Morbi non quam nec dui luctus rutrum. Nulla tellus.', year: 2010, url: 'http://reuters.com/accumsan/odio/curabitur/convallis.html', people: 'Victor--Gomez' };
+    const mockProject = { campus_ids: [1, 3], category_ids: [2, 3], discipline_ids: [1, 2], tags: ['definition', 'Versatile'], id: 'fe4cb1d025bd7c4bbd7845ffd36a2f2045b246f5', title: 'Tin', description: 'Donec diam neque, vestibulum eget, vulputate ut, ultrices vel, augue. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec pharetra, magna vestibulum aliquet ultrices, erat tortor sollicitudin mi, sit amet lobortis sapien sapien non mi. Integer ac neque.\n\nDuis bibendum. Morbi non quam nec dui luctus rutrum. Nulla tellus.', year: 2010, url: 'http://reuters.com/accumsan/odio/curabitur/convallis.html', people: 'Victor--Gomez' };
+    const mockPeople = 'Victor--Gomez';
+    const mockPeopleObjects = [{ name_first: 'Victor', name_last: 'Gomez' }];
+    const mockCampuses = [
+      { id: 1, name: 'IU Bloomington' },
+      { id: 2, name: 'IU East' },
+      { id: 3, name: 'IU Kokomo' },
+    ];
+    const mockCampusIds = [1, 3];
+    const mockCampusNames = ['IU Bloomington', 'IU Kokomo'];
+    const mockCategories = [
+      { id: 1, name: 'Virtual Reality' },
+      { id: 2, name: 'Augmented Reality' },
+      { id: 3, name: '3D Digitization' },
+    ];
+    const mockCategoryIds = [2, 3];
+    const mockCategoryNames = ['Augmented Reality', '3D Digitization'];
+    const mockDisciplines = [
+      { id: 1, name: 'Science and Mathematics' },
+      { id: 2, name: 'Engineering and Technology' },
+      { id: 3, name: 'Informatics and Computing' },
+    ];
+    const mockDisciplineIds = [1, 2];
+    const mockDisciplineNames = ['Science and Mathematics', 'Engineering and Technology'];
 
     // The injector ignores leading and trailing underscores here (i.e. _$httpBackend_).
     // This allows us to inject a service and assign it to a variable with the same name
@@ -24,9 +47,63 @@ describe('projectDetail', () => {
     it('should get project', () => {
       expect(ctrl.project).toEqual({});
       $httpBackend.expectGET(`${url}/project/${ctrl.projectId}`).respond(mockProject);
-      ctrl.getProject();
       $httpBackend.flush();
       expect(ctrl.project).toEqual(mockProject);
+    });
+
+    it('should create peopleObjects', () => {
+      expect(ctrl.peopleObjects).toEqual([]);
+      ctrl.project.people = mockPeople;
+      ctrl.createPeopleObjects();
+      expect(ctrl.peopleObjects).toEqual(mockPeopleObjects);
+    });
+
+    it('should get campuses', () => {
+      expect(ctrl.campuses).toEqual([]);
+      $httpBackend.expectGET(`${url}/project/${ctrl.projectId}`).respond(mockProject);
+      $httpBackend.expectGET(`${url}/campuses`).respond(mockCampuses);
+      $httpBackend.flush();
+      expect(ctrl.campuses).toEqual(mockCampuses);
+    });
+
+    it('should create campusNames', () => {
+      expect(ctrl.campusNames).toEqual([]);
+      ctrl.campuses = mockCampuses;
+      ctrl.project.campus_ids = mockCampusIds;
+      ctrl.createCampusNames();
+      expect(ctrl.campusNames).toEqual(mockCampusNames);
+    });
+
+    it('should get categories', () => {
+      expect(ctrl.categories).toEqual([]);
+      $httpBackend.expectGET(`${url}/project/${ctrl.projectId}`).respond(mockProject);
+      $httpBackend.expectGET(`${url}/categories`).respond(mockCategories);
+      $httpBackend.flush();
+      expect(ctrl.categories).toEqual(mockCategories);
+    });
+
+    it('should create categoryNames', () => {
+      expect(ctrl.categoryNames).toEqual([]);
+      ctrl.categories = mockCategories;
+      ctrl.project.category_ids = mockCategoryIds;
+      ctrl.createCategoryNames();
+      expect(ctrl.categoryNames).toEqual(mockCategoryNames);
+    });
+
+    it('should get disciplines', () => {
+      expect(ctrl.disciplines).toEqual([]);
+      $httpBackend.expectGET(`${url}/project/${ctrl.projectId}`).respond(mockProject);
+      $httpBackend.expectGET(`${url}/disciplines`).respond(mockDisciplines);
+      $httpBackend.flush();
+      expect(ctrl.disciplines).toEqual(mockDisciplines);
+    });
+
+    it('should create disciplineNames', () => {
+      expect(ctrl.disciplineNames).toEqual([]);
+      ctrl.disciplines = mockDisciplines;
+      ctrl.project.discipline_ids = mockDisciplineIds;
+      ctrl.createDisciplineNames();
+      expect(ctrl.disciplineNames).toEqual(mockDisciplineNames);
     });
   });
 });
