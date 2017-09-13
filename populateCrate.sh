9ff23a61c73c72a52d63b9d2a562350988abefc2
pdbeard <pdbeard@iu.edu>
2017-09-13 13:52:11 -0400

    Add more generic crate populate script

--- populateCrate.sh
+++ populateCrate.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-~/builds/crate-1.1.1/bin/crash -c "COPY showcase.projects from '$(pwd)/sql/MOCK_DATA.json'"
-~/builds/crate-1.1.1/bin/crash -c "COPY showcase.campuses from '$(pwd)/sql/campuses.json'"
-~/builds/crate-1.1.1/bin/crash -c "COPY showcase.disciplines from '$(pwd)/sql/disciplines.json'"
-~/builds/crate-1.1.1/bin/crash -c "COPY showcase.categories from '$(pwd)/sql/categories.json'"
-
-
+/usr/share/crate/bin/crash < ./sql/schemas.sql
+/usr/share/crate/bin/crash -c "COPY showcase.projects from '$(pwd)/sql/MOCK_DATA.json'"
+/usr/share/crate/bin/crash -c "COPY showcase.campuses from '$(pwd)/sql/campuses.json'"
+/usr/share/crate/bin/crash -c "COPY showcase.disciplines from '$(pwd)/sql/disciplines.json'"
+/usr/share/crate/bin/crash -c "COPY showcase.categories from '$(pwd)/sql/categories.json'"
